d27ac1e988e672b6d3cd0991383c762945070fe9
Rit Chakraborty <65804779+Rit-Chakraborty@users.noreply.github.com>
2020-09-08 13:49:03 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width,initial-scale=1.0">
-		<title>Mclaren's Pub</title>
+		<title>My Folio</title>
 		<link rel="stylesheet" href="./includes/bootstrap/css/bootstrap.css" />
 		<link rel="stylesheet" href="./includes/bootstrap/css/bootstrap.min.css" />
 		<link rel="stylesheet" href="./includes/bootstrap/css/bootstrap-grid.css" />
@@ -25,13 +25,13 @@
 			<div class="nav navbar mx-auto text-center bg-dark mb-3"> 
 				<div class=" navbar-brand disabled"> 
 					<a href="#" class="btn disabled"> 
-						<h2 class="text-white">  McLaren's Pub </h2>
+						<h2 class="text-white">  Rit Chakraborty </h2>
 					</a>
 				</div>
 			</div>
 			
 			<div class="card mx-auto text-center">
-				It's gonna be LEGEN wait for it DARY . Legendary !
+				He is LEGEN wait for it DARY . Legendary !
 			</div>
 			
 		</div>
